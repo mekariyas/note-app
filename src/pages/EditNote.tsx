f6a452be235738eed6f0bb94616968f5f39bf2e6
Mekariyas <mekariyas05@gmail.com>
2025-07-14 15:48:19 +0300

    fixed typescript type error in editNotePage

--- src/pages/EditNote.tsx
+++ src/pages/EditNote.tsx
@@ -1,4 +1,5 @@
-import { useEffect,useState, FormEvent } from 'react'
+import { useEffect,useState } from 'react'
+import type { FormEvent } from "react"
 import { useParams, useNavigate} from "react-router-dom"
 import useFormStorage from "../store/store"
 
@@ -27,7 +28,7 @@ const Note = () => {
 
   const handleSubmit = (e:FormEvent)=>{
     e.preventDefault()
-    if(title && body){
+    if(title && body&& note){
       note.title = title
       note.body = body
       editNotes(notes)
@@ -36,7 +37,7 @@ const Note = () => {
   }
 
   return (
-  <main className="w-full h-[85vh] md:h-[87.5vh]  flex items-center justify-center dark:bg-black dark:text-white">
+  <main className="w-full  h-[85vh] md:h-[87.5vh]  flex items-center justify-center dark:bg-black dark:text-white">
         {!note? (<h1 className="text-lg font-semibold text-red-500 mt-4 w-full text-center">Note not found</h1>) :(<form className="w-full h-full mt-6 md:mt-0 md:w-[40%] md:h-[95%] flex flex-col rounded-sm shadow-blue-400  shadow-2xl md:rounded-lg" onSubmit={handleSubmit}>
             <label className="text-lg font-semibold ml-2 mt-2 mb-2">Title:</label>
             <input type="text" value={title} placeholder="Write the title here" className="h-10 w-[90%] outline-none border-b-[1px] ml-4 pl-1 mb-2" required onChange={(e)=>setTitle(e.target.value)}/>
